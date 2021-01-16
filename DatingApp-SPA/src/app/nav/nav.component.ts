0adc5b7d14bc4e9876f19a18aa7545151dc34942
Yashasvishukla <35268031+Yashasvishukla@users.noreply.github.com>
2021-01-16 10:13:31 +0530

    Login and Register

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.authService.login(this.model).subscribe(resonse => {
+      console.log('logged in successfully');
+    }, error => {
+      console.log('error occured while login');
+    });
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+  }
+}
