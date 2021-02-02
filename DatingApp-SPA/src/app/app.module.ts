8c24b59e2f5a00c8ba9378229bf1c86232235d98
Yashasvishukla <35268031+Yashasvishukla@users.noreply.github.com>
2021-02-02 20:18:04 +0530

    UI Changes for Member-Detail

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,11 +11,24 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BsDropdownModule } from 'ngx-bootstrap';
-import { MemberListComponent } from './member-list/member-list.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { MemeberDetailComponent } from './members/memeber-detail/memeber-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemeberListResolver } from './_resolvers/member-list.resolver';
+import { AuthGuard } from './_guards/auth.guard';
+import { NgxGalleryModule } from 'ngx-gallery-9';
+
+export function tokenGetter(){
+  return localStorage.getItem('token');
+}
+
 @NgModule({
   declarations: [						
     AppComponent,
@@ -24,7 +37,9 @@ import { RouterModule } from '@angular/router';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemeberDetailComponent
    ],
   imports: [
     BrowserModule,
@@ -32,11 +47,23 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
+    JwtModule.forRoot({
+      config : {
+        tokenGetter: tokenGetter,
+        allowedDomains : ['localhost:5000'],
+        disallowedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthGuard,
+    MemberDetailResolver,
+    MemeberListResolver
   ],
   bootstrap: [AppComponent]
 })
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
 import {Routes} from '@angular/router';
 import { HomeComponent} from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemeberDetailComponent } from './members/memeber-detail/memeber-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemeberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
     {path : '', component : HomeComponent},
@@ -13,7 +16,8 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path : 'members' , component : MemberListComponent},
+            { path : 'members' , component : MemberListComponent , resolve: {users: MemeberListResolver}},
+            {path : 'members/:id' , component : MemeberDetailComponent , resolve: {user : MemberDetailResolver}},
             { path : 'lists' , component : ListsComponent},
             { path : 'messages' , component : MessagesComponent},
         ]
